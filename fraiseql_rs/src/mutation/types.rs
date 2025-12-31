6d116ced43a8125d1974eb0343c1948e2e9cd028
Lionel Hamayon <lionel.hamayon@evolution-digitale.fr>
2025-12-31 23:29:36 +0100

    docs: document mutation/types.rs methods (2 items)
    - StatusKind::is_success() - Check if status is success
    - StatusKind::is_error() - Check if status is error

--- fraiseql_rs/src/mutation/types.rs
+++ fraiseql_rs/src/mutation/types.rs
@@ -89,11 +89,13 @@ impl StatusKind {
         }
     }
 
+    /// Check if this status is a success variant
     #[must_use]
     pub const fn is_success(&self) -> bool {
         matches!(self, Self::Success(_))
     }
 
+    /// Check if this status is an error variant
     #[must_use]
     pub const fn is_error(&self) -> bool {
         matches!(self, Self::Error(_))
